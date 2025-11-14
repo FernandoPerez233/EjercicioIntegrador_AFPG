ea44c0857244589e707b365faead62b15b7ece0b
FernandoPerez233 <al45722@un.edu.mx>
2025-11-13 22:37:37 -0600

    final changes

--- script.js
+++ script.js
@@ -29,7 +29,53 @@ function CalcularTotal(){
     else if(analisis == "ACCconsulta"){
         total += 1000;
     }
+    else{
+        alert("Seleccione otro tipo de servicios");
+    }
+
+    var dinamica = formulario.querySelector('input[name="dinamica"]:checked').value;
+
+    if(dinamica == "receta"){
+        total += 1000;
+    }
+    else if(dinamica == "ejercicios"){
+        total += 1000;
+    }
+    else if(dinamica == "revista"){
+        total += 150;
+    }
+    else{
+        alert("Seleccione un tipo de preparación");
+    }
+
+    var tipodepago = document.getElementById("tipodepago").value;
+
+    if(tipodepago == "efectivo"){
+        total = total * 1.0;
+    }
+    else if(tipodepago == "tarjetacod"){
+        total = total +(total * 0.06);
+    }
+    else if(tipodepago == "transferencia"){
+        total = total + (total * 0.03) ;
+    }
+    else{
+        alert("Seleccione un tipo de pago");
+    }
+
+    var FAC = document.getElementById("factura").value;
+    if(FAC == "si"){
+        total = total + (total * 0.16);
+    }
+    else if(FAC == "no"){
+        total = total * 1.0;
+    }
+    else{
+        alert("Seleccione si desea factura o no");
+    }
 
     document.getElementById("total").innerHTML = "Total: $ " + total;
+
+    alert("Gracias por su interes en nuestros servicios.");
     
 }
